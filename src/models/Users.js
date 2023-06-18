9f16ea6681981b12b0068d54fc0628fa2d107bbb
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2023-06-18 10:33:43 -0300

    Criptografia de senha

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,24 +1,37 @@
 const db = require('./db');
+const bcrypt = require('bcryptjs');
 
 const Users = db.sequelize.define('Users', {
-    id_user: {
-        type: db.Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    nick: {
-        type: db.Sequelize.STRING
-    },
-    email: {
-        type: db.Sequelize.STRING
-    },
-    pwd: {
-        type: db.Sequelize.STRING
-    }
-    
+  id_user: {
+    type: db.Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  nick: {
+    type: db.Sequelize.STRING
+  },
+  email: {
+    type: db.Sequelize.STRING
+  },
+  pwd: {
+    type: db.Sequelize.STRING
+  }
 }, { freezeTableName: true });
 
-//Users.sync({ force: true });
+// Antes de salvar o usuário no banco de dados, criptografa a senha
+Users.beforeCreate(async (user) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(user.pwd, salt);
+  user.pwd = hash;
+});
+
+// Verifica se a senha fornecida pelo usuário corresponde à senha criptografada no banco de dados
+Users.prototype.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.pwd);
+};
+
+// Comentado para evitar recriação da tabela
+//Users.sync({ force: true }); 
 
 module.exports = Users;
